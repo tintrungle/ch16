88c0674858bb282bcf6b898e1046cc38d1d9bcdf
Elton Stoneman <elton_stoneman@tjx.com>
2025-02-20 12:23:39 +0000

    Add Windows package & arch tags

--- cicd/package.ps1
+++ cicd/package.ps1
@@ -1,8 +1,21 @@
 
 try {
+    $info = docker version -f json | ConvertFrom-Json
+    $env:DOCKER_BUILD_OS = $info.Server.Os.ToLower()
+    $env:DOCKER_BUILD_CPU = $info.Server.Arch.ToLower()
+    $env:OS_VERSION_TAG=''
+    if ($env:DOCKER_BUILD_OS -eq 'windows') {
+        $env:OS_VERSION_TAG="-ltsc2022"
+    }
+
+    $composeFiles = @(
+        '-f', 'docker-compose.yml',
+        '-f', 'docker-compose-package.yml'
+    )
+
     pushd ../src
-    docker compose build --pull
-    docker compose push
+    docker compose $composeFiles build --pull
+    docker compose $composeFiles push
 }
 finally {
     popd
