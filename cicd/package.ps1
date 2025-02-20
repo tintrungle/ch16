b0522fc5e299b47e26e8831da9049871ba5e6074
Elton Stoneman <elton@sixeyed.com>
2025-02-20 22:16:37 +0000

    Debug

--- cicd/package.ps1
+++ cicd/package.ps1
@@ -1,20 +1,9 @@
 
 try {
-    $info = docker version -f json | ConvertFrom-Json
-    $env:DOCKER_BUILD_OS = $info.Server.Os.ToLower()
-    $env:DOCKER_BUILD_CPU = $info.Server.Arch.ToLower()
-    $env:OS_VERSION_TAG=''
-    if ($env:DOCKER_BUILD_OS -eq 'windows') {
-        $env:OS_VERSION_TAG="-ltsc2022"
-    }
-
-    $composeFiles = @(
-        '-f', 'docker-compose.yml',
-        '-f', 'docker-compose-package.yml'
-    )
-
+    . ./set-vars.ps1
     pushd ../src
-    docker compose $composeFiles build --pull
+    docker pull diamol/node:2e
+    docker compose $composeFiles build accesslog
     docker compose $composeFiles push
 }
 finally {
---
+++ cicd/set-vars.ps1
@@ -0,0 +1,13 @@
+
+    $info = docker version -f json | ConvertFrom-Json
+    $env:DOCKER_BUILD_OS = $info.Server.Os.ToLower()
+    $env:DOCKER_BUILD_CPU = $info.Server.Arch.ToLower()
+    $env:OS_VERSION_TAG=''
+    if ($env:DOCKER_BUILD_OS -eq 'windows') {
+        $env:OS_VERSION_TAG="-ltsc2022"
+    }
+
+    $composeFiles = @(
+        '-f', 'docker-compose.yml',
+        '-f', 'docker-compose-package.yml'
+    )
