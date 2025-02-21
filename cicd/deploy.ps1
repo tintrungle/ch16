1319c36c764d516f488b18a3add798849c864c42
Elton Stoneman <elton@sixeyed.com>
2025-02-21 22:17:27 +0000

    TEst helm deploy

---
+++ cicd/deploy.ps1
@@ -0,0 +1,18 @@
+try {
+    . ./set-vars.ps1
+    pushd ../src
+
+    $releaseSpec = docker compose $releaseComposeFiles config --format json | ConvertFrom-Json
+
+    popd
+    pushd ../helm/iotd
+
+    helm install iotd `
+        --set api.image = $releaseSpec.services.'image-of-the-day'.image `
+        --set log.image = $releaseSpec.services.'access-log'.image `
+        --set web.image = $releaseSpec.services.'image-gallery'.image `
+        .
+}
+finally {
+    popd
+}
